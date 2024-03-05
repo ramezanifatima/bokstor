dd72e0b16651aa0716a6275eb45097af2ce51746
ramezanifatima <ramezanifatemeh988@gmail.com>
2024-03-05 00:54:17 -0800

    edit

--- API/views.py
+++ API/views.py
@@ -1,27 +1,25 @@
 from django.shortcuts import render
-
-from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.response import Response
-from django.contrib.auth.models import User
-
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import mixins
 from . models import Customer, Book
 from .serializers import CustomersSerializers, BookSerializers, BookCustomerSerializers
-from  rest_framework import viewsets
-from rest_framework.decorators import action
+from rest_framework.permissions import BasePermission
+
+class CustomPermission(BasePermission):
+    def has_permission(self, request, view):
+        book_id = view.kwargs['pk']
+        if request.user.is_authenticated:
+            if Book.objects.filter(pk = book_id,customer=request.user):
+                BookViewsets.serializer_class=BookCustomerSerializers
+                return True
+        return False
 
 
-class BookViewsets(viewsets.ViewSet):
-        def list (self,request):
-            queryset = Book.objects.all()
-            serializer = BookSerializers(queryset,many=True)
-            return Response(serializer.data)
-        def retrieve(self,request,pk=None):
-            if not request.user.is_authenticated:
-                return Response({'massage':'pleas login!!'})
-            product = Book.objects.get(pk=pk)
-            customer = request.user.customer
-            if customer.book.filter(pk=pk).exists():
-                serializer = BookCustomerSerializers(product)
-                return Response(serializer.data)
-            else:
-                return Response({'massage':"in mahsol baray shoma da dastres nist"})
+class BookViewsets(mixins.ListModelMixin,
+                   mixins.RetrieveModelMixin,
+                   GenericViewSet
+                   ):
+       queryset = Book.objects.all()
+       serializer_class = BookSerializers
+       permission_classes = [CustomPermission]
